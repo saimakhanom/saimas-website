92581987a10bd64f137f502fd818e5b0241fd80e
Saima Khanom <100773828+saimakhanom@users.noreply.github.com>
2022-08-30 17:24:29 +0100

    footer date dynamic

--- src/Home/Footer.js
+++ src/Home/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => { 
 	return ( 
 		<footer>
-			<p className="footer">Copyright © 2022 - All Rights Reserved - Saima Khanom </p>
+			<p className="footer">Copyright © {(new Date().getFullYear())} - All Rights Reserved - Saima Khanom </p>
 		</footer>
 	); 
 };
